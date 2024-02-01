cd56a891ed63d5c51d6b097cf45192a307bf0488
Udaypawar004 <111646210+Udaypawar004@users.noreply.github.com>
2024-02-01 16:27:58 +0000

    added some changes in pom.xml file and modified app config file

--- ChatApplication/chat-app/src/main/java/com.app.config/AppConfig.java
+++ ChatApplication/chat-app/src/main/java/com.app.config/AppConfig.java
@@ -1,10 +1,12 @@
-import java.util.Arrays;
+    import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
@@ -24,7 +26,7 @@ public class AppConfig{
         .and().authorizeHttpRequests(authorize->authorize.requestMatchers("/api/**").authenticated().anyRequest().permitAll()
         ).addFilterBefore(null, null)
         .csrf().disable()
-        .cors().ConfigurationSource(new CorsConfigurationSource() {
+        .cors().configurationSource((org.springframework.web.cors.CorsConfigurationSource) new CorsConfigurationSource() {
 
             @Override
             public CorsConfiguration getCorsConfiguration(ServerWebExchange arg0) {
@@ -35,14 +37,21 @@ public class AppConfig{
                 corsConfig.setAllowedMethods(Collections.singletonList("*"));
                 corsConfig.setAllowCredentials(true);
                 corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+                corsConfig.setExposedHeaders(Arrays.asList("Authorization"));
+                corsConfig.setMaxAge(3600L);
 
-
-                throw new UnsupportedOperationException("Unimplemented method 'getCorsConfiguration'");
+                // throw new UnsupportedOperationException("Unimplemented method 'getCorsConfiguration'");
+                return corsConfig;
             }
             
         })
         .and().formLogin().and().httpBasic();
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 } 
